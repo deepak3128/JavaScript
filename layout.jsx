572023e9fb6ec018ea945c2d3ddf84bfce1da4ed
Deepak Katiyar <deepakhappy16@mpgi.edu.in>
2024-08-28 13:41:10 +0530

    layout.jsx

--- layout.jsx
+++ layout.jsx
@@ -1,5 +1,4 @@
-// This is the root layout component for your Next.js app.
-// Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
+
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
