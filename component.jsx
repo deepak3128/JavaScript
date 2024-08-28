9b3c1dfdb8b841a45ea7c1fd7d34d3934883dfdd
Deepak Katiyar <deepakhappy16@mpgi.edu.in>
2024-08-28 13:42:43 +0530

    component.jsx

--- component.jsx
+++ component.jsx
@@ -1,8 +1,4 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/e6eOi6si1ek
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
+
 import Link from "next/link"
 
 export default function Component() {
@@ -11,7 +7,7 @@ export default function Component() {
       <header className="px-4 lg:px-6 h-14 flex items-center bg-background border-b">
         <Link href="#" className="flex items-center justify-center" prefetch={false}>
           <MountainIcon className="h-6 w-6" />
-          <span className="sr-only">John Doe</span>
+          <span className="sr-only">Deepak Katiyar</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link
@@ -45,7 +41,7 @@ export default function Component() {
         </div>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-muted">
-        <p className="text-xs text-muted-foreground">&copy; 2024 John Doe. All rights reserved.</p>
+        <p className="text-xs text-muted-foreground">&copy; 2024 Deepak Katiyar All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link href="#" className="text-xs hover:underline underline-offset-4 text-muted-foreground" prefetch={false}>
             Privacy Policy
